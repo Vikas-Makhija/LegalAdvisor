0ca54079dbcf5f2005f721a7716ab173c10f7a0e
Vikas-Makhija <vikasmakhija07@gmail.com>
2024-11-17 17:01:21 +0530

    Add project files.

---
+++ LegalApp/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.ChatCompletion;
+using OllamaSharp;
+namespace LegalApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+           
+            builder.Services.AddScoped<IOllamaApiClient>(_ => new OllamaApiClient("http://localhost:11434"));
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddSingleton<HttpClient>();
+            builder.Services.AddScoped<IChatCompletionService, OllamaChatCompletionService>();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
